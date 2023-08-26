dce1d2af0a03cccf0308887f4892461ac3dc0adb
Ristirianto Adi <ristiriantoadi@gmail.com>
2023-08-27 06:19:10 +0700

    create UI habit card

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import { faSearch } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Button, Container, Form } from "react-bootstrap"
+import { Button, Card, Container, Form } from "react-bootstrap"
 
 function Home() {
   return (
@@ -14,6 +14,59 @@ function Home() {
               <FontAwesomeIcon style={{display:"inline",position: "absolute",right: "5px",top: "0px",zIndex: "9999"}} icon={faSearch} />
             </div>
           </div>
+        </div>
+        <div style={{margin:"30px 0"}}>
+          <Card style={{marginBottom:"30px"}}>
+            <Card.Body style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
+              <div style={{width:"50%",display:"flex",alignItems:"center",justifyContent:"space-around"}}>
+                <span style={{marginRight:"5px",minWidth:"80px",textAlign:"center"}}>Lari Pagi</span>
+                <div style={{marginRight:"5px",minWidth:"80px",textAlign:"center"}}>
+                  <input className="form-check-input" type="checkbox" id="flexCheckDefault" style={{width:"30px",height:"30px",marginRight:"10px"}}></input>
+                </div>
+                <div style={{marginRight:"5px",minWidth:"80px",textAlign:"center"}}>
+                  <label style={{fontWeight:"600",display:"block"}}>Streak</label>
+                  <span>0</span>
+                </div>
+                <div style={{marginRight:"5px",minWidth:"80px",textAlign:"center"}}>
+                  <label style={{fontWeight:"600",display:"block"}}>Goal</label>
+                  <span>30</span>
+                </div>
+                <div style={{marginRight:"5px",minWidth:"80px",textAlign:"center"}}>
+                  <label style={{fontWeight:"600",display:"block"}}>Estimated End Date</label>
+                  <span>10 September 2023</span>
+                </div>
+              </div>
+              <span style={{color:"#03A9F4",textAlign:"center",width:"150px"}}>
+                POSITIVE
+              </span>
+            </Card.Body>
+          </Card>
+          <Card style={{marginBottom:"30px"}}>
+            <Card.Body style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
+              <div style={{width:"50%",display:"flex",alignItems:"center"}}>
+                <span style={{marginRight:"30px",padding:"0 20px"}}>Lari Pagi</span>
+                {/* <div style={{marginRight:"30px",display:"flex",alignItems:"center"}}>
+                  <input className="form-check-input" type="checkbox" id="flexCheckDefault" style={{width:"30px",height:"30px",marginRight:"10px"}}></input>
+                  <h6 style={{margin:"0px"}}>20</h6>
+                </div> */}
+                <div style={{marginRight:"30px",marginBottom:"0",padding:"0 20px",textAlign:"center"}}>
+                  <label style={{fontWeight:"600",display:"block"}}>Streak</label>
+                  <span>0</span>
+                </div>
+                <div style={{marginRight:"30px",marginBottom:"0",padding:"0 20px",textAlign:"center"}}>
+                  <label style={{fontWeight:"600",display:"block"}}>Goal</label>
+                  <span>30</span>
+                </div>
+                <div style={{marginRight:"30px",marginBottom:"0",padding:"0 20px",textAlign:"center"}}>
+                  <label style={{fontWeight:"600",display:"block"}}>Estimated End Date</label>
+                  <span>10 September 2023</span>
+                </div>
+              </div>
+              <span style={{color:"#FFC400",textAlign:"center",width:"150px"}}>
+                NEGATIVE
+              </span>
+            </Card.Body>
+          </Card>
         </div>
     </Container>
   )
