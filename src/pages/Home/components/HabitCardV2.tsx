9fe1b8eb3e0cd7f51bca9f89e12f9914bdfdad7d
Ristirianto Adi <ristiriantoadi@gmail.com>
2023-08-27 17:34:19 +0700

    fix ui card

--- src/pages/Home/components/HabitCardV2.tsx
+++ src/pages/Home/components/HabitCardV2.tsx
@@ -20,7 +20,7 @@ function HabitCardV2({habit}:Props) {
     <div>
         <Card className={`${style.small} ${style.card}`}>
             <Card.Body style={{display:"flex",justifyContent:"space-evenly",alignItems:"center"}}>
-                {habit.type === "positive" ? <input style={{width:"30px",height:"30px",marginRight:"10px"}} className={`form-check-input`} type="checkbox"></input>:<button style={{color:"#D50000",border:"none",backgroundColor: "inherit",width:"30px",height:"30px"}}><FontAwesomeIcon  icon={faRefresh}/></button>}
+                {habit.type === "positive" ? <input style={{width:"30px",height:"30px",marginRight:"10px"}} className={`form-check-input`} type="checkbox"></input>:<button style={{color:"#D50000",border:"none",backgroundColor: "inherit"}}><FontAwesomeIcon style={{width:"30px",height:"30px"}}  icon={faRefresh}/></button>}
                 <div style={{width:"70%"}}>
                     <div style={{display:"flex",justifyContent:"space-between",alignItems:"center",height:"30px"}}>
                         <span style={{marginRight:"15px",width:"300px",whiteSpace: "nowrap",overflow: "hidden",textOverflow: "ellipsis"}}>{habit.name}</span>
@@ -49,28 +49,22 @@ function HabitCardV2({habit}:Props) {
         </Card>
         <Card className={`${style.big} ${style.card}`}>
             <Card.Body style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
-                <div style={{display:"flex",alignItems:"center",width:"70%",justifyContent:"space-between"}}>
-                    <div style={{width:"80%",display:"flex"}}>
-                        <div style={{display:"flex",justifyContent:"flex-start",alignItems:"center"}}>
-                            <span style={{marginRight:"20px",width:"150px",whiteSpace: "nowrap",overflow: "hidden",textOverflow: "ellipsis"}}>{habit.name}</span>
-                            {habit.type === "positive" ? <input style={{width:"20px",height:"20px",marginRight:"10px"}} className={`form-check-input`} type="checkbox"></input>:<button style={{color:"#D50000",border:"none",backgroundColor: "inherit"}}><FontAwesomeIcon style={{"width":"20px",height:"20px"}} icon={faRefresh}/></button>}
-                        </div>
-                        <div style={{display:"flex",marginLeft:"60px",width:"60%",minWidth:"400px"}}>
-                            <div style={{display:"flex", marginRight:"60px",flexFlow:"column"}}>
-                                <label style={{fontWeight:"600"}}>Streak</label>
-                                <span>{habit.streak.toString()}</span>
-                            </div>
-                            <div style={{display:"flex",flexFlow:"column",marginRight:"60px",}}>
-                                <label style={{fontWeight:"600"}}>Goal</label>
-                                <span>{habit.goal.toString()}</span>
-                            </div>
-                            <div style={{display:"flex",flexFlow:"column",marginRight:"60px",}}>
-                                <label style={{fontWeight:"600"}}>Estimation Date</label>
-                                <span>{habit.estimationDate}</span>
-                            </div>
-                        </div>
+                <div style={{display:"flex",alignItems:"center"}}>
+                    {habit.type === "positive" ? <input style={{width:"30px",height:"30px",marginRight:"10px"}} className={`form-check-input`} type="checkbox"></input>:<button style={{color:"#D50000",border:"none",backgroundColor: "inherit"}}><FontAwesomeIcon style={{"width":"30px",height:"30px"}} icon={faRefresh}/></button>}
+                    <span style={{marginLeft:"20px",width:"150px",whiteSpace: "nowrap",overflow: "hidden",textOverflow: "ellipsis"}}>{habit.name}</span>
+                    <div style={{display:"flex", marginLeft:"20px",width:"150px",flexFlow:"column"}}>
+                        <label style={{fontWeight:"600"}}>Streak</label>
+                        <span>{habit.streak.toString()}</span>
                     </div>
-                    {habit.type == "positive"? <span className={style.positive}>Positive Habit</span>:<span className={style.negative}>Negative Habit</span>} 
+                    <div style={{display:"flex", marginLeft:"20px",width:"150px",flexFlow:"column"}}>
+                        <label style={{fontWeight:"600"}}>Goal</label>
+                        <span>{habit.goal.toString()}</span>
+                    </div>
+                    <div style={{display:"flex", marginLeft:"20px",width:"150px",flexFlow:"column"}}>
+                        <label style={{fontWeight:"600"}}>Estimation Date</label>
+                        <span>{habit.estimationDate}</span>
+                    </div>
+                    {habit.type == "positive"? <span className={style.positive} style={{marginLeft:"60px",width:"150px"}}>Positive Habit</span>:<span className={style.negative} style={{marginLeft:"60px",width:"150px"}}>Negative Habit</span>}
                 </div>
                 <div style={{display:"flex"}}>
                     <button style={{border:"none",backgroundColor: "inherit",marginRight:"20px"}}><FontAwesomeIcon style={{width:"25px","height":"25px",color:"#007BFF"}} icon={faEdit}></FontAwesomeIcon></button>
