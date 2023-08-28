443fc5aae4367f2aa30f661f3834c87dfafdaa87
Ristirianto Adi <ristiriantoadi@gmail.com>
2023-08-28 19:04:18 +0700

    logout

--- src/components/NavbarPrivate.tsx
+++ src/components/NavbarPrivate.tsx
@@ -1,8 +1,15 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { auth } from '../FirebaseConfig';
 
 function NavbarPrivate() {
+
+  const logout = ()=>{
+    console.log("sign out")
+    return auth.signOut()
+  }
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
       <Container className='container-fluid' >
@@ -10,7 +17,7 @@ function NavbarPrivate() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Logout</Nav.Link>
+            <Nav.Link href="#home" onClick={logout}>Logout</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
